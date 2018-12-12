6309fd6faaa0cc03c41c006aa3b78a0848aab825
mmmaaaggg <mmmaaaggg@163.com>
2018-12-12 14:26:32 +0800

    add ignore requirements

---
+++ fast_install.bat
@@ -0,0 +1,10 @@
+pip install d:\Softwares\DevTools\Python\numpy-1.15.3-cp36-none-win_amd64.whl
+pip install d:\Softwares\DevTools\Python\numdifftools-0.9.20-py2.py3-none-any.whl
+pip install d:\Softwares\DevTools\Python\matplotlib-3.0.1-cp36-cp36m-win_amd64.whl
+pip install d:\Softwares\DevTools\Python\statsmodels-0.9.0-cp36-cp36m-win_amd64.whl
+pip install d:\Softwares\DevTools\Python\pyflux-0.4.15.tar.gz
+pip install d:\Softwares\DevTools\Python\seaborn-0.9.0-py3-none-any.whl
+pip install d:\Softwares\DevTools\MySQL\mysqlclient-1.3.12-cp36-cp36m-win_amd64.whl
+pip install d:\Softwares\DevTools\Python\pandas-0.23.0-cp36-cp36m-win_amd64.whl
+pip install d:\Softwares\DevTools\Python\SQLAlchemy-1.2.8-cp36-cp36m-win_amd64.whl
+pip install -r requirements.txt
