a4a87e5a797276455288886a7ea626c31e1fc01d
mmmaaaggg <mmmaaaggg@163.com>
2018-12-12 14:37:20 +0800

    add backend

---
+++ ifaf/backend/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 2018/6/12 13:47
+@File    : __init__.py.py
+@contact : mmmaaaggg@163.com
+@desc    : 
+"""
+from sqlalchemy import create_engine, MetaData
+from ifaf.config import config
+
+engine_dic = {key: create_engine(url) for key, url in config.DB_URL_DIC.items()}
+engine_md = engine_dic[config.DB_SCHEMA_MD]
